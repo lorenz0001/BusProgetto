d708aa27d6b1c88c1eda357586907647a4f66894
ferdy07-git <ferdydeangeliss@gmail.com>
2025-02-18 22:14:54 +0100

    Add files via upload

---
+++ frontend/sito migliorato con css e js esterni/chi siamo/chi_siamo.js
@@ -0,0 +1,9 @@
+function goHome() {
+    window.location.href = "../homePage/homePage.html"; 
+}
+function goService(){
+    window.location.href = "/supporto clienti/supportoClienti.html";
+}
+function goForum(){
+    window.location.href = "/forum/forum.html";
+}
